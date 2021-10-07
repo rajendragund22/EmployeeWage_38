06aa71f65954fbaef99d31689c47f3c7929f4003
RajendraGund <rajendragund2015@gmail.com>
2021-10-08 01:33:44 +0530

    Added : Program for the calculate the wages till a condition of total working hours

--- EmpWage.js
+++ EmpWage.js
@@ -5,6 +5,7 @@ const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS = 160;
 
 function getWorkingHrs(employeeCheck) {
     switch (employeeCheck) {
@@ -18,12 +19,14 @@ function getWorkingHrs(employeeCheck) {
 }
 
 let totalEmpHrs = 0;
-for (let day = 1; day <= MAX_WORKING_DAYS; day++) {
+let totalWorkigDays = 0;
+while (totalEmpHrs < MAX_WORKING_HOURS && totalWorkigDays < MAX_WORKING_DAYS) {
     let employeeCheck = Math.floor(Math.random() * 10) % 3;
     let emphrs = getWorkingHrs(employeeCheck);
     totalEmpHrs += emphrs;
-
+    totalWorkigDays++;
 }
 
 let totalEmpWage = totalEmpHrs * WAGE_PER_HR;
-console.log(" Total Employee Wage is: " + totalEmpWage + " for Total Employee Work Hours: " + totalEmpHrs);
+console.log(" Total Employee Wage is: " + totalEmpWage + " for Total Employee Work Hours: " + totalEmpHrs
++" Total Working Days: "+totalWorkigDays);
