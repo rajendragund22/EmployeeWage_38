805373d65f9b1b7f1a382612d5bbbb2d49278be4
RajendraGund <rajendragund2015@gmail.com>
2021-10-08 01:24:38 +0530

    Added : Program for the Refactor the code to write a function

--- EmpWage.js
+++ EmpWage.js
@@ -1,22 +1,28 @@
-const IS_ABSENT=0;
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HRS=4;
-const FULL_TIME_HRS=8;
-const WAGE_PER_HOUR=20;
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
 
-let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
 
-switch(empCheck){
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HRS;
-        break;
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HRS;
-        break;
-    default:
-        empHrs=0;
+function getWorkingHrs(employeeCheck) {
+    switch (employeeCheck) {
+        case IS_PART_TIME:
+            console.log("Employee Worked Part Time");
+            empHrs = PART_TIME_HRS;
+            break;
+        case IS_FULL_TIME:
+            console.log("Employee Worked Full Time");
+            empHrs = FULL_TIME_HRS;
+            break;
+        default:
+            console.log("Employee is Absent");
+            empHrs = 0;
+    }
 }
-        let empWage=empHrs*WAGE_PER_HOUR;
-        console.log("Employee Wage is : "+empWage);
+
+let employeeCheck = Math.floor(Math.random() * 10) % 3;
+getWorkingHrs(employeeCheck);
+let empWage = empHrs * WAGE_PER_HR;
+console.log("Employee Wage is: " + empWage);
