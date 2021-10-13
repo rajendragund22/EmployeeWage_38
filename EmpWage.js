65611fa63216e7f1e1f240b33e998de340d01088
RajendraGund <rajendragund2015@gmail.com>
2021-10-13 16:30:49 +0530

    Added : Program for the calculate Daily and Total wage into the employee wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,3 +1,4 @@
+
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -6,6 +7,9 @@ const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 const MAX_WORKING_DAYS = 20;
 const MAX_WORKING_HOURS = 160;
+let empDailyWageArray = new Array();
+let totalEmpHrs = 0;
+let totalWorkigDays = 0;
 
 function getWorkingHrs(employeeCheck) {
     switch (employeeCheck) {
@@ -18,15 +22,18 @@ function getWorkingHrs(employeeCheck) {
     }
 }
 
-let totalEmpHrs = 0;
-let totalWorkigDays = 0;
-while (totalEmpHrs < MAX_WORKING_HOURS && totalWorkigDays < MAX_WORKING_DAYS) {
-    let employeeCheck = Math.floor(Math.random() * 10) % 3;
-    let emphrs = getWorkingHrs(employeeCheck);
-    totalEmpHrs += emphrs;
-    totalWorkigDays++;
-}
 
+while (totalEmpHrs < MAX_WORKING_HOURS && totalWorkigDays < MAX_WORKING_DAYS) {
+    totalWorkigDays++;
+    let employeeCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHrs(employeeCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArray.push(calculateWage(empHrs));
+}
+function calculateWage(empHrs) {
+    return empHrs * WAGE_PER_HR;
+}
 let totalEmpWage = totalEmpHrs * WAGE_PER_HR;
-console.log(" Total Employee Wage is: " + totalEmpWage + " for Total Employee Work Hours: " + totalEmpHrs
-+" Total Working Days: "+totalWorkigDays);
+console.log("Daily Employee Wage stored in an Array:\n" + empDailyWageArray);
+console.log(" Total Employee Wage : " + totalEmpWage + " Total Employee Work Hours: " + totalEmpHrs
+    + " Total Working Days: " + totalWorkigDays);
